fa1199b99503d99041db631284d27b5717582af9
bz699 <zeebha@gmail.com>
2020-05-09 21:23:22 +0200

    header change

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ function App() {
     <div className="App">
       <header className="App-header">
       <p>
-          Run Chaos !
+          Run chaos
         </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <Morpion />
         <a
           className="App-link"
           href="https://reactjs.org"
